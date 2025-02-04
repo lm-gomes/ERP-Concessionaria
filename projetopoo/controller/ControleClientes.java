84433931d05ad771e8daf492288b9916d6006bfc
JadielSilva11 <jdielcunha@gmail.com>
2025-02-04 14:02:53 -0300

    Commit
    Push da classe ControleClientes

---
+++ projetopoo/controller/ControleClientes.java
@@ -0,0 +1,14 @@
+package controller;
+import java.util.Scanner;
+import modelos.Cliente;
+
+public class ControleClientes{
+    public static void cadastrarClientes(Cliente c){
+        try(BufferedWriter bw = new BufferedWriter(new fileWriter("data/clientes.txt", true))){
+
+        }
+        catch(Exception e){
+
+        }
+    }
+}
