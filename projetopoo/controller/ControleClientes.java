5c30bf8fab25fab710d6734aeaac73a4984bc408
JadielSilva11 <jdielcunha@gmail.com>
2025-02-05 13:27:07 -0300

    Commit
    Push da class ControleCliente

--- projetopoo/controller/ControleClientes.java
+++ projetopoo/controller/ControleClientes.java
@@ -1,14 +1,17 @@
 package controller;
-import java.util.Scanner;
 import modelos.Cliente;
+
+import java.util.Scanner;
+import java.io.*;
 
 public class ControleClientes{
     public static void cadastrarClientes(Cliente c){
-        try(BufferedWriter bw = new BufferedWriter(new fileWriter("data/clientes.txt", true))){
-
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("data/clientes.txt", true))){
+            bw.write("NOME:" + c.getNome() + ", ENDERECO: " + c.getEndereco() + ", CONTATO:" + c.getContato()); 
+            bw.newLine();
         }
         catch(Exception e){
-
+            e.printStackTrace();
         }
     }
 }
