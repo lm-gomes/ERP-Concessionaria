f88f01baae9fe027e1fe9ae7dc94e5e38428b4bb
JadielSilva11 <jdielcunha@gmail.com>
2025-02-07 18:00:12 -0300

    Update clientes.java

--- projetopoo/modelos/Cliente.java
+++ projetopoo/modelos/Cliente.java
@@ -1,14 +1,23 @@
 package modelos;
 
 public class Cliente {
-    private String nome;  
+    private String nome;
+    private int cpf;  
     protected String endereco;
     protected int contato;
 
-    public Cliente(String nome, String endereco, int contato){
+    public Cliente(String nome, int cpf, String endereco, int contato){
         this.nome = nome;
+        this.cpf = cpf;
         this.endereco = endereco;
         this.contato = contato;
+    }
+
+    public void setCpf(int cpf){
+        this.cpf = cpf;
+    }
+    public int getCpf(){
+        return cpf;
     }
 
     public void setNome(String nome){
