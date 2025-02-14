36eb50d4fa349c0ae2b6bf2677446e52d157ceec
JadielSilva11 <jdielcunha@gmail.com>
2025-02-14 16:33:18 -0300

    delete Main

--- projetopoo/modelos/Moto.java
+++
@@ -1,7 +0,0 @@
-package modelos;
-
-public class Moto extends Veiculo {
-    public Moto(int quantidade, String marca, String modelo, int ano, double preco, String cor,double km){
-        super(quantidade, marca, modelo, ano, preco, cor, km);
-    }
-}
